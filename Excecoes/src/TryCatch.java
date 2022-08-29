4b5f6208cf39c93a4a60055ccc70759b4ea1b7a4
Guihii <guihiirodrigues@gmail.com>
2022-08-29 14:27:32 -0300

    try/catch

--- Excecoes/src/TryCatch.java
+++ Excecoes/src/TryCatch.java
@@ -1,8 +1,6 @@
 import java.util.*;
-public class TryCatch {
-  
 
-   
+public class TryCatch {
     public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner  (System.in).useLocale(Locale.US);
 
